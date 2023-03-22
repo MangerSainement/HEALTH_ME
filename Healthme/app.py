9e5da7cdc16f2ed67e8228a7e0a031ea70f05a04
PHantasm111 <94317933+PHantasm111@users.noreply.github.com>
2023-03-22 09:54:44 +0100

    3.22 PH

--- Healthme/app.py
+++ Healthme/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3, os, datetime
 
-app = Flask(__name__)
+def connect_to_db():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    return conn, c
 
+def excute_query(query):
+    conn, c = connect_to_db()
+    c.execute(query)
+    conn.commit()
+    conn.close()
+    return c.fetchall()
+
+
+app = Flask(__name__)
 
 # page d'acceuil---------------------------------------------------------------------------
 @app.route('/')
@@ -39,6 +51,19 @@ def inscription():
         Allergie = request.form['allergie']
         Email = request.form['email']
         MotDePass = request.form['motdepass']
+        enregistrement = request.form['enregistrement']
+
+        if enregistrement == "oui":
+            enregistrement = "1"
+        else:
+            enregistrement = "0"
+
+        if Pseudo == "" or Sexe == "" or DtNaissance == "" or Intolerane == "" or Allergie == "" or Email == "" or MotDePass == "":
+            return render_template("Inscription.html", error="Veuillez remplir tous les champs")
+        # save the user's info to database (if he wants)
+        # selon le type de syptome, chercher le type de recette et chercher le type de aliment
+        # Créer un écran de confirmation, puis passer à l'écran qui doit être affiché.
+        return redirect(url_for('page_confirmation'))
 
     return render_template("Inscription.html")
 
