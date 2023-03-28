e28c23aad4eb2ec4cdb53a369cb12fb23429cb9c
PHantasm111 <94317933+PHantasm111@users.noreply.github.com>
2023-03-28 18:17:55 +0200

    3.28 PH

--- Healthme/app.py
+++ Healthme/app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify
-import sqlite3, os, datetime
+import sqlite3
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify, flash
+
+app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 
 def connect_to_db():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    return conn, c
 
-def excute_query(query):
+
+
+def execute_query(query):
     conn, c = connect_to_db()
     c.execute(query)
     conn.commit()
     conn.close()
     return c.fetchall()
 
-
-app = Flask(__name__)
 
 # page d'acceuil---------------------------------------------------------------------------
 @app.route('/')
@@ -41,55 +43,70 @@ def recette():
 def inscription():
     # recuperer des info de utlisateur
     if request.method == 'POST':
-        Pseudo = request.form["Pseudo"]
-        Sexe = request.form["sexe"]
-        DtNaissance = request.form["Dt"]
-        Intolerane = request.form["intolerance"]
-        # Si l'utilisateur choisit
-        if Intolerane == "Autres":
-            Intolerane = request.form["Autre_Intolerance"]
-        Allergie = request.form['allergie']
+        pseudo = request.form["Pseudo"]
+        sex = request.form["sex"]
+        dtNaissance = request.form["Dt"]
+        intolerance = request.form["intolerance"]
+
+        # Si l'utilisateur choisit "Autres" en 'intolerance'
+        if intolerance == "Autres":
+            intolerance = request.form["Autre_Intolerance"]
+        allergie = request.form['allergie']
         traitement_maladie = request.form['maladie']
-        # Si l'utilisateur choisit autre dans la formulaire'
+
+        # Si l'utilisateur choisit "Autres" dans la formulaire'
         if traitement_maladie == "Autres":
             traitement_maladie = request.form['Autre_Maladie']
-        Email = request.form['email']
-        MotDePass = request.form['motdepass']
-        enregistrement = request.form['enregistrement'
 
-        # si l'utilisateur choisit enregistrement, on enregistre ses informations dans la base de donnees
-        if enregistrement == "oui":
-            enregistrement = "1"
-        else:
-            enregistrement = "0"
+        email = request.form['email']
+        motDePass = request.form['motdepass']
+        enregistrement = request.form['enregistrement']
 
         # Verifier si tous les champs sont remplis
-        if Pseudo == "" or Sexe == "" or DtNaissance == "" or Intolerane == "" or Allergie == "" or Email == "" or MotDePass == "":
+        if pseudo == "" or sex == "" or dtNaissance == "" or intolerance == "" or allergie == "" or email == "" or motDePass == "":
             return render_template("Inscription.html", error="Veuillez remplir tous les champs")
 
-        # Verifier si l'utilisateur existe dans la base de données'
-        query_existant = f"""
+
+        # ------------------------------- Vérification l'adresse email est Unique --------------------------------------
+        # Vérifier que l'adresse électronique de l'utilisateur existe déjà dans la base de données.
+        # L'adresse électronique de l'utilisateur doit être unique en tant que nom de compte pour la connexion.
+        dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='XE')
+        conn = cx_Oracle.connect(user='PH', password='123456', dsn=dsn_tns)
+
+        query_existant = f""" 
             select *
             from Client
-            where EmailC = {Email}
+            where EmailC = {email}
         """
 
-        existant = excute_query(query_existant)
-        if len(existant) > 0:
-            return render_template("Inscription.html", error="Cet utilisateur existe déjà")
+        res_query_existant = execute_query(query_existant)
+        # --------------------------------- Flash Messaging System ---------------------------------------------------
+        # On utilise Flash pour rendre un message dans le page inscription.
+        if res_query_existant:
+            flash('Cette adresse e-mail est déjà enregistrée.')
+            return redirect('/inscription.html')
+        # [Manque] HTML code - block pour recevoir ce message
+        # <...>
 
-        
 
+        # Inscription réussie...
+        # ==>
+        # Stocker des donnees
+
+
+        # ------------------------------- Stockage des info --------------------------------------
         # Stocker l'utilisateur dans la base de données'
-        user = {'useremail': Email, 'MotDePass': MotDePass}
-        users.append(user)
-        session['useremail'] = Email
+        # si l'utilisateur choisit "enregistrement", on enregistre ses informations dans la base de donnees
+        if enregistrement == "oui":
+
+
 
         # save the user's info to database (if he wants)
         # selon le type de syptome, chercher le type de recette et chercher le type de aliment
         # Créer un écran de confirmation, puis passer à l'écran qui doit être affiché.
         return redirect(url_for('page_confirmation'))
 
+    # page d'inscription
     return render_template("Inscription.html")
 
 
